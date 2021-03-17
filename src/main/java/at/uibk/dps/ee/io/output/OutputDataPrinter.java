feb7ea2c22a59aff7ae26227dc8168c3908dca42
Fedor <fedor@dps.uibk.ac.at>
2021-03-17 08:39:21 +0100

    output via logger

--- src/main/java/at/uibk/dps/ee/io/output/OutputDataPrinter.java
+++ src/main/java/at/uibk/dps/ee/io/output/OutputDataPrinter.java
@@ -4,6 +4,8 @@ import com.google.gson.JsonObject;
 import com.google.inject.Singleton;
 
 import at.uibk.dps.ee.core.OutputDataHandler;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * The {@link OutputDataPrinter} simply prints the enactment result to the
@@ -15,9 +17,12 @@ import at.uibk.dps.ee.core.OutputDataHandler;
 @Singleton
 public class OutputDataPrinter implements OutputDataHandler {
 
+  protected final Logger outputLogger = LoggerFactory.getLogger(OutputDataPrinter.class);
+  
   @Override
   public void handleOutputData(final JsonObject outputData) {
-    System.out.println("Enactment finished");
-    System.out.println("Enactment result: " + outputData.toString());
+    outputLogger.info("Workflow executed correctly.");
+    outputLogger.info("Enactment result: {}", outputData.toString());
   }
 }
+
--- src/test/java/at/uibk/dps/ee/io/output/OutputDataPrinterTest.java
+++ src/test/java/at/uibk/dps/ee/io/output/OutputDataPrinterTest.java
@@ -2,27 +2,36 @@ package at.uibk.dps.ee.io.output;
 
 import static org.junit.Assert.*;
 
-import java.io.ByteArrayOutputStream;
-import java.io.PrintStream;
-
+import java.util.List;
 import org.junit.Test;
-
 import com.google.gson.JsonObject;
+import ch.qos.logback.classic.Level;
+import ch.qos.logback.classic.Logger;
+import ch.qos.logback.classic.spi.ILoggingEvent;
+import ch.qos.logback.core.read.ListAppender;
 
 public class OutputDataPrinterTest {
 
 	@Test
 	public void test() {
-		final ByteArrayOutputStream outputStreamCaptor = new ByteArrayOutputStream();
-		System.setOut(new PrintStream(outputStreamCaptor));
-
 		JsonObject testInput = new JsonObject();
 		testInput.addProperty("Prop1", true);
 		testInput.addProperty("Prop2", 3);
-
+		
 		OutputDataPrinter tested = new OutputDataPrinter();
+		Logger logger = (Logger) tested.outputLogger;
+		ListAppender<ILoggingEvent> listAppender = new ListAppender<>();
+		listAppender.start();
+		logger.addAppender(listAppender);
+		
 		tested.handleOutputData(testInput);
-		String expected = "Enactment finished\nEnactment result: " + testInput.toString();
-		assertEquals(expected, outputStreamCaptor.toString().trim());
+		String expected1 = "Workflow executed correctly.";
+		String expected2 = "Enactment result: " + testInput.toString();
+		    
+		List<ILoggingEvent> logList = listAppender.list;
+		assertEquals(expected1, logList.get(0).getFormattedMessage());
+		assertEquals(Level.INFO, logList.get(0).getLevel());
+		assertEquals(expected2, logList.get(1).getFormattedMessage());
+		assertEquals(Level.INFO, logList.get(1).getLevel());
 	}
 }
