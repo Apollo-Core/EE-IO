ce125148c746fb3f1713ec5da22226f27ad66fda
Fedor <fedor@dps.uibk.ac.at>
2021-04-21 07:37:29 +0200

    module to set the logging config

---
+++ src/main/java/at/uibk/dps/ee/io/modules/LoggingModule.java
@@ -0,0 +1,27 @@
+package at.uibk.dps.ee.io.modules;
+
+import org.opt4j.core.config.annotations.Info;
+import org.opt4j.core.config.annotations.Order;
+import at.uibk.dps.ee.guice.modules.OutputModule;
+import ch.qos.logback.classic.util.ContextInitializer;
+
+public class LoggingModule extends OutputModule {
+
+  @Order(1)
+  @Info("Path of the file configuring the loggers.")
+  public String pathToConfigFile = "./logging/config/logback.xml";
+
+  @Override
+  protected void config() {
+    // configure the location of the logback config file
+    System.setProperty(ContextInitializer.CONFIG_FILE_PROPERTY, pathToConfigFile);
+  }
+
+  public String getPathToConfigFile() {
+    return pathToConfigFile;
+  }
+
+  public void setPathToConfigFile(String pathToConfigFile) {
+    this.pathToConfigFile = pathToConfigFile;
+  }
+}
